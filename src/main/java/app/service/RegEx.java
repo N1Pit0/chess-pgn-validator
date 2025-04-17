b82a450b6d1831fd334a54de6965669011feb956
Nika <nikolozi_matsaberidze@epam.com>
2025-04-17 11:31:26 +0400

    fixed the bug

--- src/main/java/app/service/RegEx.java
+++ src/main/java/app/service/RegEx.java
@@ -4,15 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegEx {
-    public static int check(String regex, String input) {
-        System.out.println("Regex being compiled: " + regex);
+    public static boolean check(String regex, String input) {
+        System.out.print("String being matched: " + input);
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
-        int count = 0;
-        while (matcher.find()) {
-            count++;
-        }
 
-        return count;
+        return matcher.matches();
     }
 }
